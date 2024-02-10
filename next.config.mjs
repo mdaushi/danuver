30d437216e2968c0f1761f7e4d4738eaa3442ed2
mdaushi <firdaus.ex19@gmail.com>
2024-02-10 23:14:55 +0800

    add: jotai and pouchDB

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    swcPlugins: [['@swc-jotai/react-refresh', {}]],
+  }
 }
 
 export default nextConfig
